9289012b933fd46aa4bcd10e41dc536b110cd3dc
pts2800 <nbg183@gmail.com>
2026-08-08 19:19:16 -0400

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,13 @@
+//imports mongoDB
 import mongoose from "mongoose"
+
+//attempts to connect to DB
 export const connectDB = async () => {
+    //if successfull
     try {
         await mongoose.connect('mongodb://192.168.1.163:27017')
         console.log("mongodb connected successfully")
+    //if not successful
     } catch (error) {
         console.error("eroor connecting to mongodb", error);
         process.exit(1) //exit with failure
